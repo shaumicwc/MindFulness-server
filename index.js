000a14c3e4677384419266c2914b6321198ccad4
shaumicwc <shaumic_chowdhury@hotmail.com>
2023-06-08 17:02:16 +0600

    users api added

--- index.js
+++ index.js
@@ -89,6 +89,40 @@ async function run() {
       }
   })
 
+          //users api
+          app.post('/all-users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user?.email }
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+                return res.send({})
+            }
+            const result = await userCollection.insertOne(user)
+            res.send(result)
+        })
+        app.get('/current-user', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email }
+            const result = await userCollection.findOne(query)
+            res.send(result)
+        })
+        app.get('/all-users', async (req, res) => {
+            const result = await userCollection.find().toArray()
+            res.send(result)
+        })
+        app.put('/all-users/:id', async(req, res)=>{
+            const id = req.params.id;
+            const role = req.query.role;
+            const query = {_id : new ObjectId(id)}
+            const updatedDoc ={
+                $set : {
+                    role : role
+                }
+            }
+            const result = await userCollection.updateOne(query, updatedDoc)
+            res.send(result)
+        })
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } catch (error) {
     console.log(error.message)
