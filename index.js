60134415d0433dd8ae07ed19c030957fbc947dfd
shaumicwc <shaumic_chowdhury@hotmail.com>
2023-06-08 16:37:32 +0600

    database added in mongodb

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 3000
 
-
-
-
+app.use(cors())
+app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4kfci0x.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -21,10 +24,25 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
+
     await client.db("admin").command({ ping: 1 });
+
+    const instructorsCollection = client.db('MindFulness').collection('allInstructors')
+    const classCollection = client.db('MindFulness').collection('allClasses')
+    const selectedCollection = client.db('MindFulness').collection('selectedClasses')
+    const userCollection = client.db('MindFulness').collection('users')
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } catch (error){
     console.log(error.message)
   }
 }
 run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('MindFulness is running')
+})
+
+app.listen(port, (req, res) => {
+  console.log(`server is running port ${port}`)
+})
